a2a11a061c19bfce1541694b4634cf666b21eae9
ivicakartelo <ivicakartelo@users.noreply.github.com>
2022-08-25 11:58:55 +0200

    React router 6 complet without design

--- src/components/dates.js
+++ src/components/dates.js
@@ -1,13 +1,13 @@
 import { useLocation, NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getDates } from "../database";
 
-function QueryNavLink({ ...props }) {
-  return <NavLink {...props} />; 
+function QueryNavLink({ to, ...props }) {
+  const location = useLocation();
+  return <NavLink to={to + location.search} {...props} />;
 }
 
 export default function Dates() {
   const dates = getDates();
-  
   const [searchParams, setSearchParams] = useSearchParams();
   return (
     <>
@@ -23,16 +23,29 @@ export default function Dates() {
           }}
         />
 
-      {dates.filter((date) => {
+      {dates  
+          .filter((date) => {
             const filter = searchParams.get("filter");
             if (!filter) return true;
             
             const title = date.title.toLowerCase();
             return title.startsWith(filter.toLowerCase());
 
-          }).map((date) => (
-          <QueryNavLink to={`/dates/${date.id}`} key={date.id}>
-            <h3>{date.title}</h3> 
+          })
+        .map((date) => (
+          <QueryNavLink
+          style={({ isActive }) => {
+            return {
+              display: "block",
+              margin: "1rem 0",
+              color: isActive ? "white" : "",
+              backgroundColor: isActive ? "black" : "",
+            };
+          }}
+            to={`/dates/${date.id}`}
+            key={date.id}
+          >
+            <h3>{date.title}</h3>
           </QueryNavLink>
         ))}      
         <Outlet />
